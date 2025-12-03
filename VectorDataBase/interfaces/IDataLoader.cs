25b8bcd1c5f8c695cf04907c5a359753b8381442
olofbrahm <olofbrahm@outlook.com>
2025-12-03 17:42:40 +0100

    Comments

--- VectorDataBase/interfaces/IDataLoader.cs
+++ VectorDataBase/interfaces/IDataLoader.cs
@@ -2,5 +2,10 @@ namespace VectorDataBase.Interfaces;
 
 public interface IDataLoader
 {
+    /// <summary>
+    /// Loads data from a text file
+    /// </summary>
+    /// <param name="filePath"></param>
+    /// <returns></returns>
     string[] LoadDataFromFile(string filePath);
 }
