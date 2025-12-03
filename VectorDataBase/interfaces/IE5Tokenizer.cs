8ae429ffa31d890864d5c56d9bedb78c36c4ad4e
olofbrahm <olofbrahm@outlook.com>
2025-12-03 17:42:40 +0100

    Comments

--- VectorDataBase/interfaces/IE5Tokenizer.cs
+++ VectorDataBase/interfaces/IE5Tokenizer.cs
@@ -4,5 +4,10 @@ namespace VectorDataBase.Interfaces;
 
 public interface IE5Tokenizer
 {
+    /// <summary>
+    /// Tokenizes input text into token IDs and attention mask
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
     (long[] inputIds, long[] attentionMask) Tokenize(string text);
 }
--- VectorDataBase/interfaces/IEmbeddingModel.cs
+++ VectorDataBase/interfaces/IEmbeddingModel.cs
@@ -2,6 +2,11 @@ namespace VectorDataBase.Interfaces;
 
 public interface IEmbeddingModel
 {
+    /// <summary>
+    /// Gets embeddings for the given text
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
     float[] GetEmbeddings(string text);
 
 }
