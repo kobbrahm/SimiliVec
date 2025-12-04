09a2ece70bc6886499b7e27504d752fb841bdcab
olofbrahm <olofbrahm@outlook.com>
2025-12-04 18:00:15 +0100

    Implemented API, reworked of information gets added

---
+++ SimiliVec.Api/Controllers/VectorController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using VectorDataBase.Services;
+using VectorDataBase.Datahandling;
+using System.Collections.ObjectModel;
+
+[ApiController]
+[Route("api/[controller]")]
+public class VectorController : ControllerBase
+{
+    private readonly VectorService _vectorService;
+    public VectorController(VectorService vectorService)
+    {
+        _vectorService = vectorService;
+    }
+    
+    [HttpPost("index")]
+    public IActionResult IndexDocument([FromBody] DocumentModel[] documents)
+    {
+        if(documents == null || documents.Length == 0)
+        {
+            return BadRequest("No documents provided for indexing.");
+        }
+
+        _vectorService.IndexDocument(documents);
+        return Ok($"Indexed {documents.Length} documents");
+    }
+
+    [HttpPost("search")]
+    public IActionResult Search([FromBody] string query, [FromQuery] int k = 5)
+    {
+        if(string.IsNullOrEmpty(query))
+        {
+            return BadRequest("Search query can not be empty");
+        }
+
+        var results = _vectorService.Search(query, k);
+        return Ok(results);
+    }
+}
